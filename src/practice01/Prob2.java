ca7329c171d27845838922ba1ac0e197490cabd3
oheunjuu <essenti10@naver.com>
2017-01-05 13:07:20 +0900

    practice 01

--- src/practice01/Prob2.java
+++ src/practice01/Prob2.java
@@ -4,7 +4,16 @@ package practice01;
 public class Prob2 {
 
 	public static void main(String[] args) {
-
+		for(int i=1;i<=6;i++){
+			int start=1;
+			int end=10;
+			for(int j=1;j<=10;j++){
+				System.out.println(start+" ");
+				start++;
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
--- src/practice01/Prob4.java
+++ src/practice01/Prob4.java
@@ -2,6 +2,17 @@ package practice01;
 
 public class Prob4 {
 	public static void main(String[] args) {
-
+		//String method test
+		
+		String s="Hello World";
+		System.out.println(s.length());
+		
+		int length=s.length();
+		
+		for(int i=0;i<length;i++){
+			System.out.println(s.charAt(i));
+			
+		}
+		
 	}
 }
